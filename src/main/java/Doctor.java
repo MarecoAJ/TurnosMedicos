7a1acdbe9643a8b875b143564a735c8d4fa78063
MarecoAJ <alejo_mareco@hotmail.com>
2023-07-14 18:42:52 -0300

    creacion de proyecto. uso de clases y constructor

---
+++ src/main/java/Doctor.java
@@ -0,0 +1,13 @@
+public class Doctor {
+    int id;
+    String nombreCompleto;
+    String especialidad;
+    
+    Doctor(){}
+    
+    Doctor(String name){}
+    
+    public void verNombre(){
+        System.out.println("Nombre: " + nombreCompleto);
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+           Doctor doc = new Doctor();
+           doc.nombreCompleto = "gil";
+           doc.verNombre();
+    }
+}
