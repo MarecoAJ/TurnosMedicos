0c3f19a9ecb996c4f7db486493f2ee17d363f51f
MarecoAJ <alejo_mareco@hotmail.com>
2023-07-16 17:22:24 -0300

    uso de clases anidadas static

--- src/main/java/Doctor.java
+++ src/main/java/Doctor.java
@@ -1,4 +1,7 @@
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Doctor {
 
     private static int id = 0;
@@ -6,7 +9,50 @@ public class Doctor {
     private String especialidad;
     private String email;
 
-    Doctor() {}
+    ArrayList<turnosDisponibles> turnosDisponibles = new ArrayList<>();
+
+    public static class turnosDisponibles {
+
+        private int id;
+        private Date fecha;
+        private String hora;
+
+        public turnosDisponibles() {
+        }
+
+        public turnosDisponibles(Date fecha, String hora) {
+            this.fecha = fecha;
+            this.hora = hora;
+        }
+
+        public Date getFecha() {
+            return this.fecha;
+        }
+
+        public void setFecha(Date fecha) {
+            this.fecha = fecha;
+        }
+
+        public String getHora() {
+            return this.hora;
+        }
+
+        public void setHora(String hora) {
+            this.hora = hora;
+        }
+
+        public int getId() {
+            return this.id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+    }
+
+    Doctor() {
+    }
 
     Doctor(String nombreCompleto, String especialidad) {
         id++;
@@ -14,11 +60,37 @@ public class Doctor {
         this.especialidad = especialidad;
     }
 
-    public void verNombre() {
-        System.out.println("Nombre: " + nombreCompleto);
+    public String getNombreCompleto() {
+        return this.nombreCompleto;
     }
-    
-    public void verId() {
-        System.out.println("Id doctor: " + id);
-    } 
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public String getEspecialidad() {
+        return this.especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void crearTurnoDisponible(Date fecha, String hora) {
+
+        turnosDisponibles.add(new turnosDisponibles(fecha, hora));
+    }
+
+    public ArrayList<turnosDisponibles> getTurnosDisponibles() {
+
+        return turnosDisponibles;
+    }
 }
